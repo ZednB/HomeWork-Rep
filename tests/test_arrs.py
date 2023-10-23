899cf9e2e527d3e6f43524756cc8221f1eae798f
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2023-10-23 16:44:57 +0300

    Функции на 100% покрыты тестами

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,9 +3,15 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([], 1, "test") == "test"
+    assert arrs.get([0, 0, "str"], 2, "test") == "str"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 5) == []
+    assert arrs.my_slice([1, 2, 3], ) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], 0, 3) == [1, 2, 3]
+    assert arrs.my_slice([], 0, 2) == []
+
